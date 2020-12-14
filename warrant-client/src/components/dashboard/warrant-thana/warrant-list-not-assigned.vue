3d92fd27a04719162998e21c3a0d446f83367471
Arafat <anarafat13@gmail.com>
2020-12-15 01:04:42 +0600

    date bug fix

--- warrant-client/src/components/dashboard/warrant-thana/warrant-list-not-assigned.vue
+++ warrant-client/src/components/dashboard/warrant-thana/warrant-list-not-assigned.vue
@@ -31,8 +31,8 @@
                         <td class="border">{{warrant.criminal_father_name}}</td>
                         <td class="border">{{warrant.criminal_address}}</td>
                         <td class="border">{{warrant.warrant_type}}</td>
-                        <td class="border">{{warrant.arrest_warrant_to_thana}}</td>
-                        <td class="border">{{warrant.arrest_warrant_to_thana | moment("from", "now", true) }}</td>
+                        <td class="border">{{warrant.send_date | moment("ddd, MM Do YY")}}</td>
+                        <td class="border">{{warrant.arrest_warrant_received_to_thana | moment("from", "now", true) }}</td>
                         <td class="border">
                           <button @click="show_modal_func(warrant.id)" type="button"
                               class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs leading-4 font-medium rounded text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
