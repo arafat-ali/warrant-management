2b90a177eaea9288e655e2fcfa93d8a32f870d33
Susmoy <susmoy.cse@gmail.com>
2020-12-15 22:47:22 +0600

    Change icon in sidebar. Add date in SI performance DIG

--- warrant-client/src/components/dashboard/dashboard_headr/sidebar.vue
+++ warrant-client/src/components/dashboard/dashboard_headr/sidebar.vue
@@ -187,7 +187,7 @@
               <div class="w-full flex items-center text-blue-400 h-10 pl-4 hover:bg-gray-200 rounded-lg cursor-pointer">
                 
                 <svg class="h-6 w-6  mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M3 14h18m-9-4v8m-7 0h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z" />
                 </svg>
                 <span class="text-gray-700">এসআই পারফর্মেন্স</span>  
               </div>
